960d3e138ae57407cfe937202de83f0f63f61516
Florient Leonard <florient_leonard@hotmail.com>
2023-02-07 13:15:13 +0400

    Installation de mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,40 @@
 const express = require('express'); // importation d'express
+const mongoose = require('mongoose'); // importation de mongoose
 
 const app = express(); // appel de la méthode express - permet de créer une application express
 
-app.use((req, res) => { // use pour tout type de requête
+mongoose.connect('mongodb+srv://Flo974:P6974@clusterp6.zs9g2at.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // permettre d'accéder à notre API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); // permettre d'ajouter les headers mentionnés aux requêtes envoyées vers notre API
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // permettre d'envoyer des requêtes avec les méthodes mentionnées
+    next();
+  });
+
+app.use((req, res, next) => {
+    console.log('Requête reçue !');
+    next();
+});
+  
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => { // use pour tout type de requête
     res.json({ message: 'Votre requête a bien été reçue !' }); // renvoie d'une réponse en json
- });
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
+});
 
 module.exports = app; // export de l'application express pour l'utiliser dans les autres fichiers
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,47 @@
 const http = require('http'); // import du package http de node
 const app = require('./app'); // import de l'appli depuis app.js
 
-app.set('port', process.env.PORT || 3000); // indique à l'appli express sur quel port tourner
+const normalizePort = val => { // la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+    const port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      return val;
+    }
+    if (port >= 0) {
+      return port;
+    }
+    return false;
+  };
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port); // indique à l'appli express sur quel port tourner
+
+  const errorHandler = error => { // la fonction errorHandler recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+
 const server = http.createServer(app); // création du serveur avec l'appli en argument
 
-server.listen(process.env.PORT || 3000); // écoute des requêtes envoyées au serveur avec la méthode listen du serveur
+server.on('error', errorHandler);
+server.on('listening', () => { // écouteur d'évènements consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+server.listen(port); // écoute des requêtes envoyées au serveur avec la méthode listen du serveur
