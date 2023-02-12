49ee00ed8ff9cbb6a8d90db30fa7da1a8beb6981
Florient Leonard <florient_leonard@hotmail.com>
2023-02-12 21:44:12 +0400

    Ajout de la variable d'environnement secret_token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
         // On extraie le token du header Authorization de la requête entrante 
         const token = req.headers.authorization.split(' ')[1];
         // On décode le token et on vérifie s'il est valide
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
         // On extraie l'ID utilisateur du token et on le rajoute à l’objet request afin que nos différentes routes puissent l’exploiter.
         const userId = decodedToken.userId;
         req.auth = {
