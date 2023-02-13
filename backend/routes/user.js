e34b51cc1cd3e2193a333b865176da231e44982c
Florient Leonard <florient_leonard@hotmail.com>
2023-02-13 16:12:11 +0400

    Ajout de regex pour valider les entrées

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 // On importe nos controllers pour les utilisateurs
 const userCtrl = require('../controllers/user');
+const validateInput = require('../middleware/validate-input');
 
 // On crée nos routes et on indique les fonctions associées
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup',validateInput, userCtrl.signup);
+router.post('/login',validateInput, userCtrl.login);
 
 // On exporte notre rooter express pour l'utiliser dans notre application
 module.exports = router;
